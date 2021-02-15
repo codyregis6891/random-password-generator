5667a8b1dff205c6b2069913d7ec15b5bcff893a
Cody Regis <codyregis6891@yahoo.com>
2021-02-14 18:22:47 -0800

    added html, css, js files

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
